84fda803cbeff6d4ef007f91ceedc6fc1dcd67af
DELL <DELL@DESKTOP-7M9B2GK>
2023-10-17 08:53:24 +0530

    Pushing the code

---
+++ Java1/src/package1/Student.java
@@ -0,0 +1,16 @@
+package package1;
+//Class creation - PILLER 1
+public class Student {
+
+	int rollno;
+	int age;
+	public void display1()
+	{
+		System.out.println("Welcome to all of you");
+	}
+	public void display2()
+	{
+		System.out.println("Automation is very easy");
+	}
+	}
+//Main Method creation - PILLER 2
